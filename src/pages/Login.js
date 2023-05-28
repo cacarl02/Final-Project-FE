3ab5e40c3c2efb45c112fcd0628e9239a78be0b0
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-28 17:59:28 +0800

    login and signup styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,38 +41,44 @@ const Login = (props) => {
         }
     }
     return (
-        <div>
-            <div>Para PH</div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        className=''
-                        onChange={(e) => setLoginEmail(e.target.value)}
-                        placeholder='Email'
-                        value={loginEmail}
-                        type='email' 
-                    />
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">Para PH</div>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Login</h2>
+                        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email:</label>
+                                <input
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    onChange={(e) => setLoginEmail(e.target.value)}
+                                    placeholder='Email'
+                                    value={loginEmail}
+                                    type='email' 
+                                />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password:</label>
+                                <input
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    onChange={(e) => setLoginPassword(e.target.value)}
+                                    placeholder='Password'
+                                    value={loginPassword}
+                                    type='password'
+                                />
+                            </div>
+                            <span>{loginError}</span>
+                            <button type="submit" className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Login</button>
+                        </form>
+                        <div>
+                            <span className="text-sm font-light text-gray-500 dark:text-gray-400">No account yet?</span>
+                            <Link to='/signup' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Signup now!</Link>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        className=''
-                        onChange={(e) => setLoginPassword(e.target.value)}
-                        placeholder='Password'
-                        value={loginPassword}
-                        type='password'
-                    />
-                </div>
-                <span>{loginError}</span>
-                <button onClick={handleSubmit} className=''>Login</button>
-            </form>
-            <div>
-                <span>Don't have an account yet?</span>
-                <Link to='/signup'>Signup now!</Link>
             </div>
-        </div>
+        </section>
     )
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -34,48 +34,54 @@ const Signup = () => {
         }
     }
     return (
-        <>
-            <div>
-                <h2>Signup</h2>
-                <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        className=''
-                        onChange={(e) => setSignupEmail(e.target.value)}
-                        placeholder='Email'
-                        value={signupEmail}
-                        type='email' 
-                    />
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Signup</h2>
+                        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email:</label>
+                                <input
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    onChange={(e) => setSignupEmail(e.target.value)}
+                                    placeholder='Email'
+                                    value={signupEmail}
+                                    type='email' 
+                                />
+                            </div>
+                            <span>{emailError}</span>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password:</label>
+                                <input
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    onChange={(e) => setSignupPassword(e.target.value)}
+                                    placeholder='Password'
+                                    value={signupPassword}
+                                    type='password'
+                                />
+                            </div>
+                            <span>{passwordError}</span>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password Confirmation:</label>
+                                <input
+                                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                                    placeholder='Confirm Password'
+                                    value={passwordConfirmation}
+                                    type='password'
+                                />
+                            </div>
+                            <span>{passwordConfirmationError}</span>
+                            <button type="submit" className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Signup</button>
+                        </form>
+                        <div>
+                            <Link to='/' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Return to Login</Link>  
+                        </div>
+                    </div>
                 </div>
-                <span>{emailError}</span>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        className=''
-                        onChange={(e) => setSignupPassword(e.target.value)}
-                        placeholder='Password'
-                        value={signupPassword}
-                        type='password'
-                    />
-                </div>
-                <span>{passwordError}</span>
-                <div>
-                    <label>Password Confirmation:</label>
-                    <input
-                        className=''
-                        onChange={(e) => setPasswordConfirmation(e.target.value)}
-                        placeholder='Confirm Password'
-                        value={passwordConfirmation}
-                        type='password'
-                    />
-                </div>
-                <span>{passwordConfirmationError}</span>
-                <button type="submit" className=''>Signup</button>
-            </form>
             </div>
-            <Link to='/'>Return to Login</Link>
-        </>
+        </section>
     )
 }
 
