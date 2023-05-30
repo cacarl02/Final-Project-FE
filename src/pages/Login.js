fd00b4a438caa16eceed4ac5d4b3aeb015975aaa
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-30 11:15:49 +0800

    changing fetch url from localhost to render url for testing render app on login logout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = (props) => {
         e.preventDefault();
         
         try{
-            const response = await fetch('http://localhost:3001/login', {
+            const response = await fetch('https://final-project-app.onrender.com/login', {
                 method : "POST",
                 headers : {
                     "Content-type": "application/json"
